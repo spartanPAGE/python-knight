98f52d68fc669b88a976a20f0374fdf0fe543827
spartanPAGE <spartanpageagain@gmail.com>
2019-01-09 17:58:24 +0100

    add event_loop functionality, refactor loop, finalize pygame

--- app/root.py
+++ app/root.py
@@ -2,29 +2,35 @@ import sys
 import pygame
 import logging
 
+
 def init_logging(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s %(message)s'):
     logging.basicConfig(stream=stream, level=level, format=format)
     logging.info(f'initialized: logging | stream={stream} | level={level} | format={format}'.replace('|', '\n\t->'))
 
+
 def init_pygame():
     pygame.init()
     logging.info('initialized: pygame')
+
 
 def init_screen(mode = (640*2, 480*2)):
     screen = pygame.display.set_mode(mode)
     logging.info(f'initialized: screen display {mode}')
     return {'screen': screen}
 
-def event_loop():
-    pass
+
+def event_loop(game_vars):
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_vars['game_running'] = False
+
 
 def loop(game_vars):
     logging.info('initialized: game loop')
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    game_vars['game_running'] = True
+    while game_vars['game_running']:
+        event_loop(game_vars)
+
 
 def init():
     collected_variables = {}
@@ -36,10 +42,15 @@ def init():
     for f in funcs:
         collected_variables.update(f() or {})
     return collected_variables
+
+
+def finalize():
+    pygame.quit()
+
     
 def run():
     game_vars = {}
     game_vars.update(init())
-    print(game_vars.keys())
 
     loop(game_vars)
+    finalize()

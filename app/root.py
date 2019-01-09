1eeaef751dca7e6f1b0d3b6ac413323dffdbca7c
spartanPAGE <spartanpageagain@gmail.com>
2019-01-09 20:06:24 +0100

    use music and wind sound in main

--- app/root.py
+++ app/root.py
@@ -10,20 +10,29 @@ class MainScene(Scene):
     def __init__(self, game_vars):
         super().__init__(name='MAIN', game_vars=game_vars)
         self.background = GIFImage('res/scenes/main/background.gif')
+        pygame.mixer.music.load('res/scenes/main/March of the Templars.mp3')
+        pygame.mixer.music.play(-1)
+        pygame.mixer.music.set_volume(game_vars['music_volume'])
+
+        effect = pygame.mixer.Sound('res/common/wind/wind-looped.wav')
+        effect.play(loops=-1)
+
         self.display = game_vars['screen']
+
 
     def on_loop(self):
         self.background.render(self.display, (0, 0))
         pygame.display.flip()
         pass
 
+
     def on_event(self, event):
         logging.info(f'MainScene:on_event: {event}')
         if event.type == pygame.KEYDOWN:
             if event.unicode == 'q':
                 self.die()
-        
 
+        
 def initialize_scenes(scenes=[]):
     logging.info(f'initialized: scenes {[scene.name for scene in scenes]}')
     return {'scenes': scenes}
@@ -31,6 +40,7 @@ def initialize_scenes(scenes=[]):
 
 def run():
     game_vars = scaffolding.initialize_pygame(screen_mode=(1344, 704))
+    game_vars.update({'music_volume': 0.5})
     
     scenes = [MainScene(game_vars=game_vars)]
     
